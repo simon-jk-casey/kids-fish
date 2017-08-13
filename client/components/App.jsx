10ace258da367680023bb00c354e3ba5f5846771
simon-jk-casey <jesusiwasevil@gmail.com>
2017-08-13 15:44:53 +1200

    front sends license data to DB

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
+import request from 'superagent'
 
 class App extends React.Component {
   constructor (props) {
     super(props)
-    console.log(this)
-    this.state = {
-      issueDate: this.todaysDate(),
-      licenceNumber: '00000001'
-    }
+    this.state = this.getInitialState()
     this.regions = [
       'Northland',
       'Auckland/Waikato',
@@ -22,6 +19,13 @@ class App extends React.Component {
       'Otago',
       'Southland'
     ]
+  }
+
+  getInitialState () {
+    return {
+      issueDate: this.todaysDate(),
+      licenceNumber: '00000001'
+    }
   }
 
   todaysDate () {
@@ -39,25 +43,78 @@ class App extends React.Component {
     return today
   }
 
+  handleChange (evt) {
+    const { name, value } = evt.target
+    this.setState({
+      [name]: value
+    })
+    console.log(this.state)
+  }
+
+  handleSubmit (evt) {
+    console.log('triggers')
+    const {
+      name,
+      birthdate,
+      addressLineOne,
+      addressLineTwo,
+      cityTown,
+      region,
+      issueDate } = this.state
+    const issueDateArr = issueDate.split('/')
+    const licenceObject = {
+      name: name,
+      birthdate: birthdate,
+      addressLineOne: addressLineOne,
+      addressLineTwo: addressLineTwo,
+      cityTown: cityTown,
+      region: region,
+      issueDate: issueDate,
+      issueMonth: parseInt(issueDateArr[1]),
+      issueYear: parseInt(issueDateArr[2])
+    }
+    evt.preventDefault()
+    request
+      .post('http://localhost:3000/api/v1/licensing')
+      .send(licenceObject)
+      .end((err, res) => {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log('repsonse', res)
+        }
+      })
+  }
+
+  handleCancel (evt) {
+    evt.preventDefault()
+    document.getElementById('licence').reset()
+    this.state = this.getInitialState()
+  }
+
   render () {
     const { state, regions } = this
     return (
       <div className='licenceWrapper'>
-        <form className='licenceForm'>
+        <form
+          id='licence'
+          className='licenceForm'
+          onSubmit={(evt) => this.handleSubmit(evt)}>
           <div className='inputName fieldBG'>
             <label htmlFor='name'>Full Name:</label>
             <input
               id='name'
               type='text'
               name='name'
-              placeholder='enter name' />
+              onChange={(evt) => this.handleChange(evt)} />
           </div>
           <div className='inputBirthDate fieldBG'>
             <label htmlFor='birthdate'>Date of Birth:</label>
             <input
               id='birthdate'
               type='date'
-              name='birthdate' />
+              name='birthdate'
+              onChange={(evt) => this.handleChange(evt)} />
           </div>
           <div className='inputAddress fieldBG'>
             <div className='subAddress'>
@@ -65,28 +122,33 @@ class App extends React.Component {
               <input
                 id='addressLineOne'
                 type='text'
-                name='addressLineOne' />
+                name='addressLineOne'
+                onChange={(evt) => this.handleChange(evt)} />
             </div>
             <div className='subAddress'>
               <label htmlFor='addressLineTwo'>Address Line 2:</label>
               <input
                 id='addressLineTwo'
                 type='text'
-                name='addressLineTwo' />
+                name='addressLineTwo'
+                onChange={(evt) => this.handleChange(evt)} />
             </div>
             <div className='subAddress'>
               <label htmlFor='cityTown'>City/Town:</label>
               <input
                 id='cityTown'
                 type='text'
-                name='cityTown' />
+                name='cityTown'
+                onChange={(evt) => this.handleChange(evt)} />
             </div>
             <div>
               <label htmlFor='region'>Region:</label>
               <select
                 id='region'
                 defaultValue='null'
-                name='region'>
+                name='region'
+                onChange={(evt) => this.handleChange(evt)}>
+                <option value='null disabled'>Select Region</option>
                 {
                   regions.map((region, index) =>
                     <option key={index} value={region}>{region}</option>
@@ -116,6 +178,10 @@ class App extends React.Component {
               placeholder={state.licenceNumber} />
           </div>
         </form>
+        <div>
+          <button form='licence' type='submit'>submit</button>
+          <button onClick={(evt) => this.handleCancel(evt)}>cancel</button>
+        </div>
       </div>
     )
   }
